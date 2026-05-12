09b23ad1d6a0062e6d438dc3ffa924544d7604da
skellysteve88 <pell.noah@hotmail.com>
2026-05-12 11:09:58 -0400

    Fix font initialization in main.py

--- reality-pancakes/main.py
+++ reality-pancakes/main.py
@@ -12,8 +12,7 @@
     async def main():
         running = True
         while running:
-            font_path = "asset/fonts/schrift.ttf"
-            font = pygame.font.Font(font_path, 24)
+            font = pygame.font.Font("asset/fonts/schrift.ttf", 24)
                 def playdemosong()
                     # 1. Get the absolute path to the directory where THIS script is saved
                     BASE_PATH = pathlib.Path(__file__).parent.resolve()
